6a5524ebc536c5e288d86755154e9591fd08613b
Admin <ragavendhiran71@gmail.com>
2026-09-14 23:46:29 +0530

    command line removed

--- easycartproject/src/main/java/easycartproject/LoginTest.java
+++ easycartproject/src/main/java/easycartproject/LoginTest.java
@@ -20,7 +20,6 @@ public class LoginTest extends BaseClass {
 
 		loginPage.enterPassword("set@123");
 
-		// ScreenshotUtil.capture(driver, "Before_Login");
 
 		Thread.sleep(2000);
 
@@ -35,33 +34,3 @@ public class LoginTest extends BaseClass {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-// LoginPage loginPage =
-// new LoginPage(driver);
-
-//  loginPage.login("ramesh@123gmail.com","set@123" );
-
-
-
-
-//   Assert.assertTrue(
-//driver.getCurrentUrl()
-//.contains("easy-carts")
-//  );
-//  }
-
-
-
-
-
-
